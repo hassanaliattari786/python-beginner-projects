23639b8a4ed83e1266feaff2ad81fe432c1584ef
hassanaliattari786 <d2671606@gmail.com>
2026-01-20 15:57:30 +0500

    Add calculator program

---
+++ calculator.py
@@ -0,0 +1,8 @@
+
+a = float (input("Please Enter Your Number a :"))
+b = float (input ("Please Enter Your Number b :"))
+ 
+print("Sum of numbers is",a+b)
+print ("Subtraction of numbers is  ",a-b)
+print ("Multiplication of numbers is",a*b)
+print ("Division of numbers is ",a/b)
