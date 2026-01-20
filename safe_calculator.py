ae6ee3de19d0e535820a46ebce93f0b9aacacf13
hassanaliattari786 <d2671606@gmail.com>
2026-01-20 16:10:49 +0500

    Add commented safe calculator program

---
+++ safe_calculator.py
@@ -0,0 +1,8 @@
+a = float(input("Please Enter Your Number a: "))
+b = float(input("Please Enter Your Number b: "))
+
+if b == 0:
+    print("Division by zero not allowed")
+else:
+    print("Division is", a / b)
+  
