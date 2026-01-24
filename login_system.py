ad438081a94951dabd82c4583b12219223956933
hassanaliattari786 <d2671606@gmail.com>
2026-01-24 18:56:12 +0500

    Simple Python login system that authenticates user using username and password.
    This Login System is a beginner-level Python project.
    The program takes a username and password from the user and checks them against predefined credentials.
    If both username and password match, the system displays a successful login message.
    Otherwise, it shows an invalid credentials message.
    This project helps beginners understand authentication logic, user input handling, and conditional statements in Python.

---
+++ login_system.py
@@ -0,0 +1,8 @@
+name = "Hassan Ali"
+key= 1122334455
+username= input("Enter Username :") 
+password= int(input ("Enter Password :"))
+if name==username and password ==key:
+    print(" Login Successful ")
+else:
+    print ("Invalid Credentials ")
